55e4400373d2563838a984b150818cee045f5f84
M4NT3S <120930015+M4NT3S@users.noreply.github.com>
2023-09-08 17:23:53 +0200

    Create Main.java
    Added the file in which the algorithm should be present

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      try{
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        System.out.println(string);
+      } catch(Exception e){
+        System.out.println("An error was found");
+      }
+      
+    }
+}
