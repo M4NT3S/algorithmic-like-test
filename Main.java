32d807cdcb2a88e0a296d6ea3848326b9abcf4a6
M4NT3S <120930015+M4NT3S@users.noreply.github.com>
2023-09-15 17:31:02 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
       try{
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.next());
+          while(scanner.hasNext()){
+              System.out.println(scanner.next());
+          }
+        
       } catch(Exception e){
         System.out.println("An error was found ");
       }
